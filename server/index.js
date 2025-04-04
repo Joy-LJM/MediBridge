d4b270d6c33fbb9d1ca0b1753a5ff8ad0d4f1002
joy <2522702541@qq.com>
2025-04-04 17:38:04 -0400

    fix cors error

--- server/index.js
+++ server/index.js
@@ -4,14 +4,45 @@ const dotenv = require("dotenv");
 const bcrypt = require("bcrypt");
 const sgMail = require("@sendgrid/mail");
 const multer = require("multer");
+const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 const { MongoClient, ObjectId } = require("mongodb");
-require("./corsSettings");
+// require("./corsSettings");
+const authMiddleware = require("./middleware/authMiddleware");
 
 const app = express();
-
-const authMiddleware = require("./middleware/authMiddleware");
+const envFile =
+  process.env.NODE_ENV === "production"
+    ? ".env.production"
+    : ".env.development";
+dotenv.config({ path: envFile });
+//allow requests from target server based on env
+const allowedOrigin =
+  process.env.NODE_ENV === "production"
+    ? "https://medi-bridge-1.vercel.app"
+    : "http://localhost:5173";
+app.use(
+  cors({
+    origin: allowedOrigin,
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allow common methods
+    credentials: true,
+  })
+);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", allowedOrigin); // Allow frontend
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Allow common HTTP methods
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allow necessary headers
+  res.header("Access-Control-Allow-Credentials", "true"); // Allow cookies
+  next();
+});
+app.options("*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", allowedOrigin);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.sendStatus(204); // No content response for preflight
+});
 
 const port = process.env.PORT || "3000";
 
